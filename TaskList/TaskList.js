a063fe826bfa022191c430db82170620c54035cb
Rajal Parmar <rajal_a_parmar@homedepot.com>
2019-06-22 15:43:11 -0500

    adding onClearButtonCLick and filterTasks events

--- TaskList/TaskList.js
+++ TaskList/TaskList.js
@@ -23,42 +23,74 @@ const taskFunctions = {
         if(elements.taskInput.value === '') {
             alert('Add a Task!');
         }
-        // Create li element
-        const li = document.createElement('li');
-        // Add class
-        li.className = 'collection-item';
-        // Create text node and append to li
-        li.appendChild(document.createTextNode(elements.taskInput.value));
-        // Create new link element
-        const link = document.createElement('a');
-        // Add class
-        link.className = selectors.closeItem;
-        // Add icon html
-        link.innerHTML = elements.closeIcon;
-        // Append the link to li
-        li.appendChild(link);
 
-        // Append li to ul
-        elements.taskList.appendChild(li);
-
+        const listItem = helperFunctions.getListItem();
+        //Append listItem to ul
+        elements.taskList.appendChild(listItem);
         taskFunctions.clearInput();
-
         e.preventDefault();
     },
     removeTask: (e) => {
-        // if(e.target ) {
+        if (e.target.parentElement.classList.contains('delete-item')) {
+            if(confirm('Are you sure you want to delete this item?')) {
+                e.target.parentElement.parentElement.remove();
+            }
+        }
+    },
+    clearTasks: () => {
+        while(elements.taskList.firstChild) {
+            elements.taskList.removeChild(elements.taskList.firstChild);
+        }
+    },
+    filterTasks: (e) => {
+        const userInput = e.target.value.toLowerCase();
 
-        // }
+        document.querySelectorAll('.collection-item').forEach((task) => {
+            const taskTextContent = task.firstChild.textContent.toLowerCase();
+            if(taskTextContent.indexOf(userInput) != -1) {
+                console.log(taskTextContent.indexOf(userInput))
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        });
     }
+}
+const helperFunctions = {
+    addClassName: (element, elementClassName) => {
+        return element.className = elementClassName;
+    },
+    getListItem: () => {
+        //create li element 
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(elements.taskInput.value));
+        //create link element
+        const link = document.createElement('a');
+        link.className = selectors.closeItem;
+        link.innerHTML = elements.closeIcon;
+        //append link to li
+        li.appendChild(link);
+        return li;
+    },
 }
 
 const loadEventListeners = {
-    onSubmit: () => {
+    onAddTaskClick: () => {
         return elements.form.addEventListener('submit', taskFunctions.addTask);
     },
     onCancelClick: () => {
         return elements.taskList.addEventListener('click', taskFunctions.removeTask)
+    },
+    onClearButtonCLick: () => {
+        return elements.clearBtn.addEventListener('click', taskFunctions.clearTasks)
+    },
+    onFilterTasks: () => {
+        return elements.filter.addEventListener('keyup', taskFunctions.filterTasks)
     }
 }
 
-loadEventListeners.onSubmit();
+loadEventListeners.onAddTaskClick();
+loadEventListeners.onCancelClick();
+loadEventListeners.onClearButtonCLick();
+loadEventListeners.onFilterTasks();
